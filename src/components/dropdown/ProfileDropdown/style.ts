9fa628f5e340adb5e00f57fba18ba3b466af30d4
21color <lehhha0110@gmail.com>
2024-09-03 21:49:33 +0900

    feat: 파일 업로드 구현

--- src/components/dropdown/ProfileDropdown/style.ts
+++ src/components/dropdown/ProfileDropdown/style.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
 
-export const DropdownContainer = styled.div``;
+export const EmojiContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-gap: 12px;
+  max-width: 408px;
+`;
+
+export const EmojiListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 18px;
+  width: 100%;
+  align-items: center;
+`;
+
+export const EmojiIconContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 24px);
+  grid-gap: 8px;
+  width: 100%;
+`;
