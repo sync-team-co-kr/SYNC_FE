8fc897818a21962e155f9eb4e80f675c7e591c10
21color <lehhha0110@gmail.com>
2024-09-04 13:49:00 +0900

    feat: svg 최적화

--- src/components/modal/CreateTaskModal/index.tsx
+++ src/components/modal/CreateTaskModal/index.tsx
@@ -19,7 +19,6 @@ import { useTaskActions, useTaskState } from '@libs/store/task/task';
 import { useGetProjectList } from '@services/project/Project.hooks';
 import { useCreateTask } from '@services/task/Task.hooks';
 
-
 import { SELECT_STATUS } from './constants';
 import {
   ButtonGroup,
