11f303936881c612f7b02407c56c280bd2638bb2
21color <lehhha0110@gmail.com>
2024-09-06 19:50:43 +0900

    feat: icon package 추가 및 api 수정

--- src/services/task/apis.ts
+++ src/services/task/apis.ts
@@ -26,6 +26,19 @@ const extractImageUrls = (description: string | undefined): string[] => {
   return matches.map((match) => match[1]);
 };
 
+const convertBase64ToFile = (base64String: string, fileName: string): File => {
+  const byteString = atob(base64String.split(',')[1]); // base64에서 데이터 부분 추출
+  const mimeType = base64String.match(/data:(.*?);base64/)?.[1] || 'image/png';
+  const byteNumbers = new Array(byteString.length);
+
+  byteNumbers.forEach((_, i) => {
+    byteNumbers[i] = byteString.charCodeAt(i);
+  });
+
+  const byteArray = new Uint8Array(byteNumbers);
+  return new File([byteArray], fileName, { type: mimeType });
+};
+
 export const createTask = async ({ ...payload }: CreateTaskPayload) => {
   const formData = new FormData();
   const imageUrls = extractImageUrls(payload.data.description);
@@ -47,29 +60,15 @@ export const createTask = async ({ ...payload }: CreateTaskPayload) => {
 
       // base64 -> Blob 변환
       try {
-        const base64Data = imageUrl.split(',')[1];
-        const contentType =
-          imageUrl.match(/data:(.*?);base64/)?.[1] || 'image/png';
-        const byteCharacters = atob(base64Data);
-        const byteNumbers = new Array(byteCharacters.length);
-
-        /* eslint-disable no-plusplus */
-        for (let i = 0; i < byteCharacters.length; i++) {
-          byteNumbers[i] = byteCharacters.charCodeAt(i);
-        }
-
-        const byteArray = new Uint8Array(byteNumbers);
-        const imageFile = new File([byteArray], newImageName, {
-          type: contentType,
-        });
+        const imageFile = convertBase64ToFile(imageUrl, newImageName);
 
         // formData에 이미지 추가
-        formData.append(`images`, imageFile, newImageName);
+        formData.append(`images`, imageFile);
       } catch (error) {
         console.error('이미지 변환 실패', error);
       }
     } else {
-      console.error('이미지 경로가 잘못되었습니다.', imageUrl);
+      console.log('이미지가 포함되지 않았습니다');
     }
   });
 
@@ -80,15 +79,12 @@ export const createTask = async ({ ...payload }: CreateTaskPayload) => {
     startDate:
       payload.data.startDate instanceof Date ? payload.data.startDate : null,
     endDate: payload.data.endDate instanceof Date ? payload.data.endDate : null,
+    parentTaskId: payload.data.parentTaskId,
   };
 
   formData.append('data', JSON.stringify(formDataList));
 
-  return requiredJwtTokeninstance.post('/user/api/task/v1', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  return requiredJwtTokeninstance.post('/user/api/task/v1', formData);
 };
 
 export const getTaskChildren = async (taskId: number) => {
