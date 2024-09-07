cdfd63dec3b6acd4baab1bf157e88df03b2a1138
21color <lehhha0110@gmail.com>
2024-09-07 22:20:16 +0900

    feat: icon package 추가 및 api 수정

--- src/services/task/apis.ts
+++ src/services/task/apis.ts
@@ -26,17 +26,30 @@ const extractImageUrls = (description: string | undefined): string[] => {
   return matches.map((match) => match[1]);
 };
 
+const getExtensionFromMimeType = (mimeType: string): string => {
+  switch (mimeType) {
+    case 'image/jpeg':
+      return 'jpg';
+    case 'image/png':
+      return 'png';
+    case 'image/gif':
+      return 'gif';
+    case 'image/webp':
+      return 'webp';
+    default:
+      return 'png';
+  }
+};
+
 const convertBase64ToFile = (base64String: string, fileName: string): File => {
   const byteString = atob(base64String.split(',')[1]); // base64에서 데이터 부분 추출
   const mimeType = base64String.match(/data:(.*?);base64/)?.[1] || 'image/png';
+  const extension = getExtensionFromMimeType(mimeType);
   const byteNumbers = new Array(byteString.length);
 
-  byteNumbers.forEach((_, i) => {
-    byteNumbers[i] = byteString.charCodeAt(i);
-  });
-
   const byteArray = new Uint8Array(byteNumbers);
-  return new File([byteArray], fileName, { type: mimeType });
+
+  return new File([byteArray], `${fileName}.${extension}`, { type: mimeType });
 };
 
 export const createTask = async ({ ...payload }: CreateTaskPayload) => {
@@ -44,26 +57,20 @@ export const createTask = async ({ ...payload }: CreateTaskPayload) => {
   const imageUrls = extractImageUrls(payload.data.description);
   const titleImage = payload.titleimage;
   console.log('titleImage', titleImage);
-
   let updatedDescription = payload.data.description;
-
   imageUrls.forEach((imageUrl) => {
     if (imageUrl.startsWith('data:image/')) {
-      // uuid 생성 및 파일명 처리
       const uuid = randomUuid();
-      const fileName = imageUrl.split('/').pop()?.split(';')[0] || 'image.png';
-      const newImageName = `${uuid}_${fileName}`;
+      const prevFileName = 'description-image';
+      const extension = getExtensionFromMimeType(imageUrl);
 
-      // 이미지 경로 업데이트
-      const newImageUrl = `http://150.136.153.235:31585/mnt/oraclevdb/task/description/${newImageName}`;
+      const newImageName = `${uuid}_${prevFileName}`;
+      const newImageUrl = `http://150.136.153.235:31585/api/task/image?filename=/mnt/oraclevdb/task/description/${newImageName}.${extension}`;
       updatedDescription = updatedDescription?.replace(imageUrl, newImageUrl);
-
-      // base64 -> Blob 변환
       try {
-        const imageFile = convertBase64ToFile(imageUrl, newImageName);
-
-        // formData에 이미지 추가
-        formData.append(`images`, imageFile);
+        const imgFile = convertBase64ToFile(imageUrl, newImageName);
+        console.log('이미지 변환 성공', imgFile);
+        formData.append(`images`, imgFile);
       } catch (error) {
         console.error('이미지 변환 실패', error);
       }
@@ -71,7 +78,6 @@ export const createTask = async ({ ...payload }: CreateTaskPayload) => {
       console.log('이미지가 포함되지 않았습니다');
     }
   });
-
   const formDataList = {
     projectId: payload.data.projectId,
     title: payload.data.title,
@@ -81,9 +87,10 @@ export const createTask = async ({ ...payload }: CreateTaskPayload) => {
     endDate: payload.data.endDate instanceof Date ? payload.data.endDate : null,
     parentTaskId: payload.data.parentTaskId,
   };
-
-  formData.append('data', JSON.stringify(formDataList));
-
+  const jsonBlob = new Blob([JSON.stringify(formDataList)], {
+    type: 'application/json',
+  });
+  formData.append('data', jsonBlob);
   return requiredJwtTokeninstance.post('/user/api/task/v1', formData);
 };
 
