c7778167a30c5ba372ac0cd1d7031e171dcedf88
somn45 <somnsomn45@gmail.com>
2024-12-22 13:18:48 +0900

    style : 업무 속성 별 색상 주입, 그래프 스타일 보충

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -27,26 +27,60 @@ const setRadiusGraph = (isStart: boolean, isEnd: boolean) => {
   return '0';
 };
 
-const GraphContainer = styled.div<{ $isstart: boolean; $isend: boolean }>`
+const setGraphColor = (attribute: number) => {
+  if (attribute === 0) return `${vars.sementic.color.lightPurple}`;
+  if (attribute === 1) return `${vars.sementic.color.alertLightOrange}`;
+  return `${vars.sementic.color.lightGreen}`;
+};
+
+const setGraphAccentColor = (attribute: number) => {
+  if (attribute === 0) return `${vars.sementic.color.purple}`;
+  if (attribute === 1) return `${vars.sementic.color.alertOrange}`;
+  return `${vars.sementic.color.green}`;
+};
+
+const GraphContainer = styled.div<{
+  $isstart: boolean;
+  $isend: boolean;
+  $attribute: number;
+}>`
   width: calc(100% + 1px);
   height: 32px;
   padding: 8px;
-  border-right: 1px solid ${vars.sementic.color.alertOrange};
+  border-right: 1px solid ${(props) => setGraphColor(props.$attribute)};
   border-radius: ${({ $isstart, $isend }) => setRadiusGraph($isstart, $isend)};
-  background-color: ${vars.sementic.color.alertOrange};
+  background-color: ${(props) => setGraphColor(props.$attribute)};
   display: flex;
   gap: 4px;
   span {
     color: ${vars.sementic.color.black};
+    font-size: 14px;
+    font-weight: 700;
   }
 `;
 
+const GraphAccent = styled.div<{ $isgraphstart: boolean; $attribute: number }>`
+  width: 4px;
+  height: 16px;
+  background-color: ${(props) =>
+    props.$isgraphstart && setGraphAccentColor(props.$attribute)};
+`;
+
 const Graph = ({ task, gridDay }: GraphProps) => {
+  console.log(gridDay.date, task.endDate);
   return (
     <GraphContainer
       $isstart={isSameDay(gridDay.date, addDays(task.startDate, 1))}
       $isend={isSameDay(gridDay.date, task.endDate)}
+      $attribute={0}
     >
+      <GraphAccent
+        $isgraphstart={
+          isSameDay(startOfWeek(gridDay.date), gridDay.date) ||
+          isSameDay(gridDay.date, addDays(task.startDate, 1))
+        }
+        $attribute={0}
+      ></GraphAccent>
       <span>
         {(isSameDay(startOfWeek(gridDay.date), gridDay.date) ||
           isSameDay(gridDay.date, addDays(task.startDate, 1))) &&
