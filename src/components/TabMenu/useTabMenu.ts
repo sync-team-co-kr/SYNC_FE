2bfb94050b7b65fbd4b24bf92289781b630d545b
somn45 <somnsomn45@gmail.com>
2025-01-14 22:35:55 +0900

    feature : TabMenu 컴포넌트 구현

---
+++ src/components/TabMenu/useTabMenu.ts
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import TabMenuItem from './TabMenu.types';
+
+type useTabMenuType = (
+  tabMenuList: TabMenuItem[],
+) => [string, (e: React.MouseEvent<HTMLButtonElement>) => void];
+
+const useTabMenu: useTabMenuType = (tabMenuList) => {
+  const [activeTabMenu, setActiveTabMenu] = useState(
+    tabMenuList[0].tabMenuName,
+  );
+  const [currentTabMenu, setCurrentTabMenu] = useState('');
+  const navigate = useNavigate();
+
+  const activateTabMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const [expectedRoute] = tabMenuList.filter(
+      (tabMenuItem) => tabMenuItem.tabMenuName === e.currentTarget.name,
+    );
+    setActiveTabMenu(expectedRoute.tabMenuName);
+    navigate(expectedRoute.route);
+  };
+
+  useEffect(() => {
+    setCurrentTabMenu(activeTabMenu);
+  }, [activeTabMenu]);
+
+  return [currentTabMenu, activateTabMenu];
+};
+
+export default useTabMenu;
