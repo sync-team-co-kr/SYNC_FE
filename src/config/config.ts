9488ccaffdd7b2ad8ca166be8fa033d7d9cd7190
somn45 <somnsomn45@gmail.com>
2025-04-24 22:59:54 +0900

    git : 원격 저장소에 올라간 환경 변수 설정 파일 제거 및 제외

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,7 @@
 // config/config.ts
 const config = {
-  backendUrl:
-    process.env.REACT_APP_SERVER_USER_URL || 'https://150.136.153.235:30443',
-  backendProjectUrl:
-    process.env.REACT_APP_SERVER_PROJECT_URL || 'https://129.213.161.199:30443',
+  backendUrl: 'https://user.sync-team.co.kr:30443',
+  backendProjectUrl: 'https://project.sync-team.co.kr:30443',
 };
 
 export default config;
